c6b4b672db124d28fc8052f327782729e2aa5a92
ssawrav <s.sawrav@gmail.com>
2022-06-19 14:06:55 -0400

    css: move inline style to external
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

--- src/App.js
+++ src/App.js
@@ -33,39 +33,16 @@ export default function App() {
       <h2>Select Music Genre to see my Song recommendations!</h2>
       <div>
         {musicList.map((music) => (
-          <li
-            style={{
-              display: "inline-block",
-              margin: "1rem",
-              border: "3px solid black",
-              borderRadius: "1rem",
-              padding: "0.5rem",
-              backgroundColor: "lightpink",
-              cursor: "pointer"
-            }}
-            onClick={() => onClickHandler(music)}
-          >
+          <li className="genrelist" onClick={() => onClickHandler(music)}>
             {music}
           </li>
         ))}
       </div>
       <div>
         {musicDB[clickCounter].map((music) => (
-          <li
-            style={{
-              borderRadius: "1rem",
-              padding: "0.5rem",
-              backgroundColor: "lightpink",
-              display: "block",
-              textAlign: "left"
-            }}
-          >
-            <div style={{ fontSize: "30px" }}>{music.song}</div>
-            <div
-              style={{ display: "block", fontSize: "20px", fontWeight: "bold" }}
-            >
-              by {music.name}
-            </div>
+          <li className="songlist">
+            <div className="songname">{music.song}</div>
+            <div className="artistname">by {music.name}</div>
           </li>
         ))}
       </div>
